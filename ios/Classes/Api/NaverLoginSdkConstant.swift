61f240c3d266fa1b46913af2e4eefc4497d70c36
Crape Myrtle <b3xlon9@gmail.com>
2024-11-17 21:38:23 +0900

    Added: iOS - First build test

---
+++ ios/Classes/Api/NaverLoginSdkConstant.swift
@@ -0,0 +1,17 @@
+//
+//  NaverLoginSdkConstant.swift
+//  naver_login_sdk
+//
+//  Created by Lagerstroemia on 11/17/24.
+//
+
+import Foundation
+
+struct NaverLoginSdkConstant {
+    static let channelNameMethod: String = "naver_login_sdk_method"
+    static let channelNameEvent: String = "naver_login_sdk_event"
+    
+    struct Key {
+        static let initialize: String  = "initialize"
+    }
+}
--- ios/Classes/NaverLoginSdkPlugin.swift
+++ ios/Classes/NaverLoginSdkPlugin.swift
@@ -3,15 +3,21 @@ import UIKit
 
 public class NaverLoginSdkPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "naver_login_sdk", binaryMessenger: registrar.messenger())
-    let instance = NaverLoginSdkPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+      // Register method channel
+      let methodChannel = FlutterMethodChannel(name: NaverLoginSdkConstant.channelNameMethod, binaryMessenger: registrar.messenger())
+      // Register event channel
+      let eventChannel = FlutterEventChannel(name: NaverLoginSdkConstant.channelNameEvent, binaryMessenger: registrar.messenger())
+      
+      let instance = NaverLoginSdkPlugin()
+      registrar.addMethodCallDelegate(instance, channel: methodChannel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case NaverLoginSdkConstant.Key.initialize:
+        print("test...")
+    // case "getPlatformVersion":
+    //  result("iOS " + UIDevice.current.systemVersion)
     default:
       result(FlutterMethodNotImplemented)
     }
