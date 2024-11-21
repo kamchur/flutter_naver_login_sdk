dcc87cc1de5985778a7bfbd7890fa6a0b536cfa0
Crape Myrtle <b3xlon9@gmail.com>
2024-11-21 11:35:53 +0900

    Added - Create MainFolder(NaverLoginSDK)

--- packages/android/src/main/kotlin/net/lagerstroemia/naver_login_sdk/api/NaverLoginSdkProtocol.kt
+++
@@ -1,9 +0,0 @@
-package net.lagerstroemia.naver_login_sdk.api
-
-interface NaverLoginSdkProtocol {
-    fun getVersion(): String
-    fun getTokenType(): String?
-    fun getAccessToken(): String?
-    fun getRefreshToken(): String?
-    fun getExpireAt(): Any
-}
--- packages/example/android/app/src/main/kotlin/net/lagerstroemia/naver_login_sdk_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package net.lagerstroemia.naver_login_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
